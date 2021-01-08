d4e2ee770f1b335f3703ce65fecac7c53e4681d7
Vinci-GitHub <zen@v.local>
2021-01-08 17:56:38 +0900

    front package 追加

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>Home</h3>
+    <button @click="toUsers">Usersのページに行く</button>
+    <p> {{ doublecount }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  computed: {
+    doublecount() {
+      return this.$store.getters.doubleCount;
+    }
+  },
+  methods: {
+    toUsers() {
+      this.$router.push("users/1");
+    }
+  }
+};
+</script>
---
+++ frontend/src/views/Users.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h3>Users</h3>
+    <h1>きたぜ</h1>
+  </div>
+</template>
